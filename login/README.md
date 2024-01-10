827903037a496cc035da0fc780c17d586e48f746
Ren Koya <koya.ren.701@gmail.com>
2024-01-10 19:34:14 +0400

    add auth0 login

---
+++ login/README.md
@@ -0,0 +1,23 @@
+## Getting Started
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+# Auth0 × world id
+
+https://auth0.com/docs/quickstart/webapp/nextjs/interactive?download=true#add-login-to-your-application
+
+https://marketplace.auth0.com/integrations/worldcoin
+
+# login page
+
+```
+ /api/auth/login
+```
---
+++ login/app/profile-server/page.tsx
@@ -0,0 +1,18 @@
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default async function ProfileServer() {
+  const session = await getSession();
+  if (!session || !session.user) {
+    return <div>No user data available</div>;
+  }
+
+  const { user } = session;
+
+  return (
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  );
+}
